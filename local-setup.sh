469107ca70da57123a3e8b9ebbed94cc544ffc0d
Dheeraj_Reddy <ksreddy.dheeraj@gmail.com>
2023-01-17 11:57:15 +0530

    added

--- local-setup.sh
+++ local-setup.sh
@@ -2,7 +2,7 @@
 
 echo "Updating the Repos"
 sudo apt update
-echo "####################################################################################"
+echo "###################################################################################################################################################################"
 
 
 
@@ -10,7 +10,7 @@ echo "##########################################################################
 echo "Install docker && add user to docker group"
 sudo apt install docker.io -y 
 sudo usermod -a -G docker devops
-echo "####################################################################################"
+echo "###################################################################################################################################################################"
 
 
 
@@ -19,7 +19,7 @@ echo "Install Docker Compose"
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
-echo "####################################################################################"
+echo "###################################################################################################################################################################"
 
 
 
@@ -29,7 +29,7 @@ curl -L -O "https://raw.githubusercontent.com/nullpo-head/wsl-distrod/main/insta
 sudo chmod +x install.sh
 sudo ./install.sh install
 sudo /opt/distrod/bin/distrod enable
-echo "####################################################################################"
+echo "###################################################################################################################################################################"
 
 
 
@@ -37,7 +37,7 @@ echo "Install Kind to create k8 cluster"
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64
 sudo chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
-echo "####################################################################################"
+echo "###################################################################################################################################################################"
 
 
 
@@ -45,7 +45,7 @@ echo "##########################################################################
 echo "Install k3d to create k8 cluster "
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 k3d
-echo "####################################################################################"
+echo "###################################################################################################################################################################"
 
 
 
@@ -56,7 +56,7 @@ sudo apt-get install apt-transport-https --yes
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update
 sudo apt-get install helm
-echo "####################################################################################"
+echo "###################################################################################################################################################################"
 
 
 
@@ -65,4 +65,19 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-echo "####################################################################################"
+echo "###################################################################################################################################################################"
+
+
+
+
+echo "Define Config file for kind cluster"
+cat <<EOF > kind.yaml
+# three node (two workers) cluster config
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+- role: worker
+- role: worker
+EOF
+echo "###################################################################################################################################################################"
