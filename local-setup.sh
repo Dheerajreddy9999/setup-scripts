9563300eafd6916c8b45d49f6b9c9ceb3d676a41
Dheeraj_Reddy <ksreddy.dheeraj@gmail.com>
2023-01-20 16:08:05 +0530

    added line

--- local-setup.sh
+++ local-setup.sh
@@ -23,23 +23,23 @@ echo "##########################################################################
 
 
 
-# echo "Enable systemd"
-# sudo cat <<EOF > /etc/wsl.conf
-# [boot]
-# systemd=true
-# EOF
-# sudo cat /etc/wsl.conf
-# echo "###################################################################################################################################################################"
-
-
-
-
-echo "Installing systemd to enable systemctl"
-curl -L -O "https://raw.githubusercontent.com/nullpo-head/wsl-distrod/main/install.sh"
-sudo chmod +x install.sh
-sudo ./install.sh install
-sudo /opt/distrod/bin/distrod enable
+echo "Enable systemd"
+sudo cat <<EOF > /etc/wsl.conf
+[boot]
+systemd=true
+EOF
+sudo cat /etc/wsl.conf
 echo "###################################################################################################################################################################"
+
+
+
+
+# echo "Installing systemd to enable systemctl"
+# curl -L -O "https://raw.githubusercontent.com/nullpo-head/wsl-distrod/main/install.sh"
+# sudo chmod +x install.sh
+# sudo ./install.sh install
+# sudo /opt/distrod/bin/distrod enable
+# echo "###################################################################################################################################################################"
 
 
 
