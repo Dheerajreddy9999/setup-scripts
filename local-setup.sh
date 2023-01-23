019db7ec114469c0785c0e9bf3a871b5b8c4a30f
Dheeraj_Reddy <ksreddy.dheeraj@gmail.com>
2023-01-23 09:20:45 +0530

    .

--- local-setup.sh
+++ local-setup.sh
@@ -84,3 +84,14 @@ nodes:
 EOF
 sudo cat kind.yaml
 echo "##################################################################################################################################################################"
+sudo cat <<EOF > /home/devops/kind.yaml
+# three node (two workers) cluster config
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+- role: worker
+- role: worker
+EOF
+sudo cat /home/devops/kind.yaml
+echo "##################################################################################################################################################################"
