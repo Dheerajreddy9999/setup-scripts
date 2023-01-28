3e625cba38ade6ea177ddb1ca73dc6557f19f7af
Dheeraj_Reddy <ksreddy.dheeraj@gmail.com>
2023-01-28 18:50:35 +0530

    added line

--- local-setup.sh
+++ local-setup.sh
@@ -105,3 +105,13 @@ echo "##########################################################################
 echo "creating jenkins_Slave directory giving permissions to devops "
 sudo mkdir /opt/jenkins_slave
 sudo chown devops.devops /opt/jenkins_slave
+echo "##################################################################################################################################################################"
+
+
+echo "installing gcloudcli & install gke-auth plugin"
+sudo apt-get install apt-transport-https ca-certificates gnupg -y
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install google-cloud-cli -y
+sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin -y
+echo "##################################################################################################################################################################"
